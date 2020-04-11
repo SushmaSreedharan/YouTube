82bf99a29761f9629ea6534c969b1f8fb6c1c8be
Sushma Sreedharan <sushmacs15@gmail.com>
2020-04-10 18:12:11 -0700

    state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
+const API_KEY ='AIzaSyDFJTiO3naCIOz5Af3LGsQtsCI5Dfyt8qo';
 
-import App from './components/app';
+YTSearch(
+    {key:API_KEY, term:'surfboards'}, function(data){
+        console.log(data);
+    }
+    )
 
+const App = function(){
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
